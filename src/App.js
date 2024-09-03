42958284cbcd0b6454642fe4eb690e88b1794fcb
MatheusWiltner <matheuswiltner@gmail.com>
2024-09-03 19:20:10 -0300

    Realização de forms simples de email e senha

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      email: '',
+      senha: ''
+    }
+    this.trocssaEmail = this.trocaEmail.bind(this);
+  }
+
+  trocaEmail(e){
+    let valorDigitado = e.target.value;
+    this.setState({email: valorDigitado});
+  }
+
+  render(){
+    return(
+      <div>
+        <h2>Login</h2>
+        Email:
+        <input type="email" name="email" value={this.state.email} onChange={this.trocaEmail}></input><br/>
+        Senha:
+        <input type="text" name="senha" value={this.state.senha} onChange={(e) => this.setState({senha: e.target.value})}></input><br/>
+        <div>
+          <h3>{this.state.email}</h3>
+          <h3>{this.state.senha}</h3>
+        </div>
+      </div>
+      
+    )
+  }
 }
-
 export default App;
